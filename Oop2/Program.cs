f45c360c90b72adde93c712a90cd14902ea0e27d
nazlu <nazluozturk_1997.55@hotmail.com>
2021-02-07 17:37:09 +0300

    23 ocak dersi

---
+++ Oop2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Oop2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GercekMusteri musteri1 = new GercekMusteri();
+            musteri1.Id = 1;
+            musteri1.MusteriNo = "12345";
+            musteri1.Adi = "Engin";
+            musteri1.Soyadi = "Demiroğ";
+            musteri1.TcNo = "12345678910";
+
+            //Kodlama.io
+            TuzelMusteri musteri2 = new TuzelMusteri();
+            musteri2.Id = 2;
+            musteri2.MusteriNo = "54321";
+            musteri2.Sirketadi = "Kodlama.io";
+            musteri2.VergiNo = "1234567890";
+
+            //gerçek müşteri - tüzel müşteri birbirinin yerine kullanılmaz!!
+            //Solid
+
+            Musteri musteri3 = new GercekMusteri();
+            Musteri musteri4 = new TuzelMusteri();
+
+            MusteriManager musteriManager = new MusteriManager();
+            musteriManager.Add(musteri1);
+            musteriManager.Add(musteri2);
+
+
+
+        }
+    }
+}
---
+++ Oop2/TuzelMusteri.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Oop2
+{     //Coorporate
+    //miras inheritance
+    class TuzelMusteri:Musteri
+    {
+       
+        public string Sirketadi { get; set; }
+        public string VergiNo { get; set; }
+
+    }
+}
